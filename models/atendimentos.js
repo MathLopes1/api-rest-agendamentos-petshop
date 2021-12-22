3ff0a62aa28f0dd7fed0689b17fb1585dded1314
Matheus <matheus.pgm2000@gmail.com>
2021-12-22 16:13:40 -0300

    add moment e tratando datas

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,14 +1,19 @@
 //Arquivo responsável pela conexão com o banco de dados
+const moment = require('moment') //Biblioteca para trabalhar com datas
 const conexao = require('../infraestrutura/conexao')
 
 //Classe com os metodos para ações da API na nossa base de dados do MySQL
 class Atendimento{
   //Metodo para Adicionar informações na tabela atendimentos
    adiciona(atendimento){
+        //Tratando as datas
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
         //Criando Query e enviando para conexão
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query (sql, atendimento, (erro, resultados) => {
+        conexao.query (sql, atendimentoDatado, (erro, resultados) => {
             if(erro){
             console.log(erro)
           } else {
